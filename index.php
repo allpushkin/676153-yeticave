6fa5ab10e36f764cb18e850aeb840e0b3e394726
Natalia <wowmalish@gmail.com>
2018-11-18 00:47:19 +0300

    Убирает кавычки у числовых значений в массиве с товарами

--- index.php
+++ index.php
@@ -8,37 +8,37 @@ $products_list = [
     [
         'title' => '2014 Rossignol District Snowboard',
         'category' => 'Доски и лыжи',
-        'cost' => '10999',
+        'cost' => 10999,
         'image' => 'img/lot-1.jpg'
     ],
     [
         'title' => 'DC Ply Mens 2016/2017 Snowboard',
         'category' => 'Доски и лыжи',
-        'cost' => '159999',
+        'cost' => 159999,
         'image' => 'img/lot-2.jpg'
     ],
     [
         'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
         'category' => 'Крепления',
-        'cost' => '8000',
+        'cost' => 8000,
         'image' => 'img/lot-3.jpg'
     ],
     [
         'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
         'category' => 'Ботинки',
-        'cost' => '10999',
+        'cost' => 10999,
         'image' => 'img/lot-4.jpg'
     ],
     [
         'title' => 'Куртка для сноуборда DC Mutiny Charocal',
         'category' => 'Одежда',
-        'cost' => '7500',
+        'cost' => 7500,
         'image' => 'img/lot-5.jpg'
     ],
     [
         'title' => 'Маска Oakley Canopy',
         'category' => 'Разное',
-        'cost' => '5400',
+        'cost' => 5400,
         'image' => 'img/lot-6.jpg'
     ]
 ];
