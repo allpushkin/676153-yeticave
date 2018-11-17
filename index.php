ffea706a1ba88c81ad3d22809254c53b9f5e7174
Natalia <wowmalish@gmail.com>
2018-11-17 22:49:40 +0300

    Создает простой массив категорий

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ $is_auth = rand(0, 1);
 
 $user_name = 'Natalia'; // укажите здесь ваше имя
 $user_avatar = 'img/user.jpg';
+$categories = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
 ?>
 <!DOCTYPE html>
 <html lang="ru">
