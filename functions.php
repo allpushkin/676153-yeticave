26c97e53d7fa2c69ccc56dcb72e6a3c3b2f95d79
Natalia <wowmalish@gmail.com>
2018-11-22 13:33:01 +0300

    Создает лейаут и шаблон главной страницы

---
+++ functions.php
@@ -0,0 +1,27 @@
+<?php
+function include_template($name, $data) {
+$name = 'templates/' . $name;
+$result = '';
+
+if (!file_exists($name)) {
+return $result;
+}
+
+ob_start();
+extract($data);
+require $name;
+
+$result = ob_get_clean();
+
+return $result;
+}
+
+function cost_formatting($cost) {
+    $cost = ceil($cost);
+    if ($cost >= 1000) {
+        $cost = number_format($cost,0,'',' ');
+    }
+    $cost .= " ₽";
+    return $cost;
+}
+?>
