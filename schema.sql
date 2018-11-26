bc37233f83fd0dfa444b1ba42a9b8465f7de6a41
Natalia <wowmalish@gmail.com>
2018-11-26 09:56:17 +0300

    Добавляет в таблицы поля для связей

--- schema.sql
+++ schema.sql
@@ -11,7 +11,9 @@ CREATE TABLE users (
   username VARCHAR(64) NOT NULL UNIQUE,
   password VARCHAR(64) NOT NULL,
   avatar VARCHAR(255),
-  contacts TEXT NOT NULL
+  contacts TEXT NOT NULL,
+  lots INT,
+  bids INT
 );
 
 CREATE TABLE categories (
@@ -22,17 +24,22 @@ CREATE TABLE categories (
 CREATE TABLE lots (
   id INT AUTO_INCREMENT PRIMARY KEY,
   creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  author_id INT NOT NULL,
+  category_id INT NOT NULL,
   title VARCHAR(255) NOT NULL,
   description TEXT NOT NULL,
   picture VARCHAR(255) NOT NULL,
   start_price INT NOT NULL,
   completion_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  step INT NOT NULL
+  step INT NOT NULL,
+  winner_id INT
 );
 
 CREATE TABLE bids (
   id INT AUTO_INCREMENT PRIMARY KEY,
   add_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  lot_id INT NOT NULL,
+  user_id INT NOT NULL,
   bid INT NOT NULL
 );
 
