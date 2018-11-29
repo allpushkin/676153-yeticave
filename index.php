fe6613fc08e20593244343a44e6c708c2aee0843
Natalia <wowmalish@gmail.com>
2018-11-29 14:00:52 +0300

    Создает массив с параметрами для подключения и выполняет подключение к БД

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 require_once('functions.php');
 require_once('data.php');
 
+$connect = mysqli_connect($database['host'], $database['user'], $database['password'], $database['db']);
+mysqli_set_charset($connect, "utf8");
+
+
 $page_content = include_template('index.php', [
     'categories' => $categories,
     'lots' => $products_list
