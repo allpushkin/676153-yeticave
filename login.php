bbfe73df67d14466ce109c8fcddb2ec9b579aef2
Natalia <wowmalish@gmail.com>
2018-12-13 00:44:16 +0300

    Добавляет валидацию полей формы входа

--- login.php
+++ login.php
@@ -3,9 +3,36 @@ require_once('functions.php');
 require_once('data.php');
 require_once('init.php');
 
+session_start();
+
 $categories = get_categories($connect);
 
-$page_content = include_template('login.php', []);
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $enter = $_POST['enter'];
+
+    $required = [
+        'email',
+        'password'
+    ];
+    $dict = [
+        'email' => 'Электронный адрес',
+        'password' => 'Пароль'
+    ];
+    $errors = [];
+    foreach ($required as $key) {
+        if (empty($enter[$key])) {
+            $errors[$key] = 'Поле ' . $key . ' не заполнено';
+        };
+    };
+
+
+}
+
+$page_content = include_template('login.php', [
+    'enter' => $enter,
+    'errors' => $errors,
+    'dict' => $dict
+]);
 
 $layout_content = include_template('layout.php', [
     'content' => $page_content,
