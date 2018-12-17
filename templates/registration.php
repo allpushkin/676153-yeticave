2643ffa6de72470f4f87a45e7e162448f551d4cf
Natalia <wowmalish@gmail.com>
2018-12-17 11:00:49 +0300

    Добавляет ссылку на вход со страницы регистрации

--- templates/registration.php
+++ templates/registration.php
@@ -58,5 +58,5 @@
         </ul>
     <?php endif; ?>
     <button type="submit" class="button">Зарегистрироваться</button>
-    <a class="text-link" href="#">Уже есть аккаунт</a>
+    <a class="text-link" href="login.php">Уже есть аккаунт</a>
 </form>
